3e3befb4177dd88a98ffe5d6edab0adf1fb9809d
RevolutionsStudio <60020572+RevolutionsStudio@users.noreply.github.com>
2020-04-24 19:44:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 
 try:
-    TwitchKey = os.environ['TOKEN']
+    TwitchKey = os.environ['TwitchKeyDiscursif']
 except KeyError:
-  print("Exit because 'TwitchKey' not found")
+  print("Exit because 'TwitchKeyDiscursif' not found")
   exit()
 
 #os.system("ffmpeg -f x11grab -s '$INRES' -r '$FPS' -i :0.0 -f pulse -i 0 -f flv -ac 2 -ar $AUDIO_RATE -vcodec libx264 -g $GOP -keyint_min $GOPMIN -b:v $CBR -minrate $CBR -maxrate $CBR -pix_fmt yuv420p -s $OUTRES -preset $QUALITY -tune film -acodec aac -threads $THREADS -strict normal -bufsize $CBR 'rtmp://$SERVER.twitch.tv/app/$STREAM_KEY'")
