440d966a3d0550e1cbc27824a9250694c4d32bc2
RevolutionsStudio <60020572+RevolutionsStudio@users.noreply.github.com>
2020-04-24 19:45:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ except KeyError:
 
 #os.system("ffmpeg -f x11grab -s '$INRES' -r '$FPS' -i :0.0 -f pulse -i 0 -f flv -ac 2 -ar $AUDIO_RATE -vcodec libx264 -g $GOP -keyint_min $GOPMIN -b:v $CBR -minrate $CBR -maxrate $CBR -pix_fmt yuv420p -s $OUTRES -preset $QUALITY -tune film -acodec aac -threads $THREADS -strict normal -bufsize $CBR 'rtmp://$SERVER.twitch.tv/app/$STREAM_KEY'")
 
-os.system("ffmpeg"+"-i input.mp4"+" -vcodec libx264 -b:v 5M -acodec aac -b:a 256k -f flv "+TwitchKey)
+os.system("ffmpeg "+"-i input.mp4"+" -vcodec libx264 -b:v 5M -acodec aac -b:a 256k -f flv "+TwitchKey)
